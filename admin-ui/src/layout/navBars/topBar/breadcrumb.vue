bc87f2458af883381d2c083e8294e24804c6ebdb
yimo <wsyimo@qq.com>
2024-07-03 19:52:16 +0800

    更新前端框架变更

--- admin-ui/src/layout/navBars/topBar/breadcrumb.vue
+++ admin-ui/src/layout/navBars/topBar/breadcrumb.vue
@@ -8,10 +8,10 @@
     />
     <el-breadcrumb class="layout-navbars-breadcrumb-hide">
       <transition-group name="breadcrumb">
-        <el-breadcrumb-item v-for="(v, k) in state.breadcrumbList" :key="!v.meta.tagsViewName ? v.meta.title : v.meta.tagsViewName">
+        <el-breadcrumb-item v-for="(v, k) in state.breadcrumbList" :key="v.path">
           <span v-if="k === state.breadcrumbList.length - 1" class="layout-navbars-breadcrumb-span">
             <SvgIcon :name="v.meta.icon" class="layout-navbars-breadcrumb-iconfont" v-if="themeConfig.isBreadcrumbIcon" />
-            <div v-if="!v.meta.tagsViewName">{{ $t(v.meta.title) }}</div>
+            <div v-if="v.meta.title">{{ $t(v.meta.title) }}</div>
             <div v-else>{{ v.meta.tagsViewName }}</div>
           </span>
           <a v-else @click.prevent="onBreadcrumbClick(v)">
@@ -25,12 +25,14 @@
 
 <script setup lang="ts" name="layoutBreadcrumb">
 import { reactive, computed, onMounted } from 'vue'
-import { onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router'
+import { onBeforeRouteUpdate, useRoute, useRouter, RouteLocationNormalized } from 'vue-router'
 import { Local } from '/@/utils/storage'
 import other from '/@/utils/other'
 import { storeToRefs } from 'pinia'
 import { useThemeConfig } from '/@/stores/themeConfig'
 import { useRoutesList } from '/@/stores/routesList'
+import { filterTree, treeToList } from '/@/utils/tree'
+import { cloneDeep } from 'lodash-es'
 
 // 定义变量内容
 const stores = useRoutesList()
@@ -48,16 +50,15 @@ const state = reactive<BreadcrumbState>({
 
 // 动态设置经典、横向布局不显示
 const isShowBreadcrumb = computed(() => {
-  initRouteSplit(route.path)
+  initRouteSplit(route)
   const { layout, isBreadcrumb } = themeConfig.value
   if (layout === 'classic' || layout === 'transverse') return false
   else return isBreadcrumb ? true : false
 })
 // 面包屑点击时
 const onBreadcrumbClick = (v: RouteItem) => {
-  const { redirect, path } = v
+  const { redirect } = v
   if (redirect) router.push(redirect)
-  else router.push(path)
 }
 // 展开/收起左侧菜单点击
 const onThemeConfigChange = () => {
@@ -70,38 +71,60 @@ const setLocalThemeConfig = () => {
   Local.set('themeConfig', themeConfig.value)
 }
 // 处理面包屑数据
-const getBreadcrumbList = (arr: RouteItems) => {
+const getBreadcrumbList = (arr: RouteItems, path: string) => {
+  //第一次初始化时执行时,避免使用路由查找时重复执行
+  if (state.routeSplitIndex == 1) {
+    //优先使用菜单判断面包屑显示，如果找不到匹配的路由菜单，则执行旧的逻辑使用地址判断
+    let routeTree = filterTree(cloneDeep(arr), path, {
+      children: 'children',
+      filterWhere: (item: any, filterword: string) => {
+        return item.path?.toLocaleLowerCase().indexOf(filterword) > -1
+      },
+    })
+    if (routeTree.length > 0) {
+      //查找第一个匹配的路由，将其展开添加到面包屑中
+      const routeArr = treeToList([routeTree[0]])
+      if (routeArr.length > 0) {
+        routeArr.forEach((item: RouteItem, k: number) => {
+          !state.breadcrumbList.find((a) => a.path === item.path) && state.breadcrumbList.push(item)
+        })
+        //匹配不到再使用路径去匹配
+        if (state.breadcrumbList.length > 0) return
+      }
+    }
+  }
+  //不存在则使用顶级的分类
   arr.forEach((item: RouteItem) => {
     state.routeSplit.forEach((v: string, k: number, arrs: string[]) => {
       if (state.routeSplitFirst === item.path) {
         state.routeSplitFirst += `/${arrs[state.routeSplitIndex]}`
-        state.breadcrumbList.push(item)
+        !state.breadcrumbList.find((a) => a.path === item.path) && state.breadcrumbList.push(item)
         state.routeSplitIndex++
-        if (item.children) getBreadcrumbList(item.children)
+        if (item.children) getBreadcrumbList(item.children, path)
       }
     })
   })
 }
 // 当前路由字符串切割成数组，并删除第一项空内容
-const initRouteSplit = (path: string) => {
+const initRouteSplit = (toRoute: RouteLocationNormalized) => {
   if (!themeConfig.value.isBreadcrumb) return false
-  state.breadcrumbList = [] //[routesList.value[0]]
-  state.routeSplit = path.split('/')
+  state.breadcrumbList = []
+  state.routeSplit = toRoute.path.split('/')
   state.routeSplit.shift()
   state.routeSplitFirst = `/${state.routeSplit[0]}`
   state.routeSplitIndex = 1
-  getBreadcrumbList(routesList.value)
-  if (route.name === 'home' || (route.name === 'notFound' && state.breadcrumbList.length > 1)) state.breadcrumbList.shift()
+  getBreadcrumbList(routesList.value, toRoute.path)
+  if (toRoute.name === 'home' || (toRoute.name === 'notFound' && state.breadcrumbList.length > 1)) state.breadcrumbList.shift()
   if (state.breadcrumbList.length > 0)
     state.breadcrumbList[state.breadcrumbList.length - 1].meta.tagsViewName = other.setTagsViewNameI18n(<RouteToFrom>route)
 }
 // 页面加载时
 onMounted(() => {
-  initRouteSplit(route.path)
+  initRouteSplit(route)
 })
 // 路由更新时
 onBeforeRouteUpdate((to) => {
-  initRouteSplit(to.path)
+  initRouteSplit(to)
 })
 </script>
 
