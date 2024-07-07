d1eb3748af7b13e5eefb290b521f44f63b51e793
yimo <wsyimo@qq.com>
2024-07-07 15:42:53 +0800

    添加项目生成按钮

--- admin-ui/src/views/dev/dev-project/index.vue
+++ admin-ui/src/views/dev/dev-project/index.vue
@@ -1,15 +1,15 @@
 ﻿<template>
-<div class="my-layout">
+  <div class="my-layout">
     <el-card class="my-search-box" shadow="never">
       <el-row>
         <el-col :span="18" :xs="24" class="my-search-box-inputs">
           <el-form :inline="true" label-width="auto" @submit.stop.prevent>
-            <el-form-item class="my-search-box-item"  label="项目名称">
-              <el-input  clearable  v-model="state.filter.name" placeholder="" @keyup.enter="onQuery" >
+            <el-form-item class="my-search-box-item" label="项目名称">
+              <el-input clearable v-model="state.filter.name" placeholder="" @keyup.enter="onQuery">
               </el-input>
             </el-form-item>
-            <el-form-item class="my-search-box-item"  label="项目编码">
-              <el-input  clearable  v-model="state.filter.code" placeholder="" @keyup.enter="onQuery" >
+            <el-form-item class="my-search-box-item" label="项目编码">
+              <el-input clearable v-model="state.filter.code" placeholder="" @keyup.enter="onQuery">
               </el-input>
             </el-form-item>
             <el-form-item>
@@ -19,13 +19,17 @@
         </el-col>
         <el-col :span="6" :xs="24" class="my-search-box-btns">
           <el-space>
-          <el-button type="primary" v-auth="perms.add" icon="ele-Plus" @click="onAdd">新增</el-button>
+            <el-button type="primary" v-auth="perms.add" icon="ele-Plus" @click="onAdd">新增</el-button>
             <el-dropdown :placement="'bottom-end'" v-if="auths([perms.batSoftDelete, perms.batDelete])">
               <el-button type="warning">批量操作 <el-icon><ele-ArrowDown /></el-icon></el-button>
               <template #dropdown>
                 <el-dropdown-menu>
-                    <el-dropdown-item v-if="auth(perms.batSoftDelete)" :disabled="state.sels.length==0" @click="onBatchSoftDelete" icon="ele-DeleteFilled">批量软删除</el-dropdown-item>
-                    <el-dropdown-item v-if="auth(perms.batDelete)"  :disabled="state.sels.length==0" @click="onBatchDelete" icon="ele-Delete">批量删除</el-dropdown-item>
+                  <el-dropdown-item :disabled="state.sels.length == 0" @click.stop="batchGenCode"
+                    icon="ele-Position">批量生成代码</el-dropdown-item>
+                  <el-dropdown-item v-if="auth(perms.batSoftDelete)" :disabled="state.sels.length == 0"
+                    @click="onBatchSoftDelete" icon="ele-DeleteFilled">批量软删除</el-dropdown-item>
+                  <el-dropdown-item v-if="auth(perms.batDelete)" :disabled="state.sels.length == 0" @click="onBatchDelete"
+                    icon="ele-Delete">批量删除</el-dropdown-item>
                 </el-dropdown-menu>
               </template>
             </el-dropdown>
@@ -35,45 +39,48 @@
     </el-card>
 
     <el-card class="my-fill mt8" shadow="never">
-      <el-table v-loading="state.loading" :data="state.devProjectListData" row-key="id"  ref="listTableRef" @row-click="listTableToggleSelection"  @selection-change="selsChange">
-        
-          <el-table-column type="selection" width="50" />
-          <el-table-column prop="name" label="项目名称" show-overflow-tooltip width />
-          <el-table-column prop="code" label="项目编码" show-overflow-tooltip width />
-          <el-table-column prop="isEnable" label="是否启用" show-overflow-tooltip width />
-          <el-table-column prop="groupId_Text" label="使用模板组" show-overflow-tooltip width />
-          <el-table-column prop="remark" label="备注" show-overflow-tooltip width />
-          <el-table-column v-auths="[perms.update,perms.softDelete,perms.delete]" label="操作" :width="actionColWidth" fixed="right">
-            <template #default="{ row }">
-              <el-button v-auth="perms.update" icon="ele-EditPen"  text type="primary" @click.stop="onEdit(row)">编辑</el-button>
-              <el-dropdown v-if="authAll([perms.delete,perms.softDelete])">
-                <el-button icon="el-icon--right" text type="danger" >操作 <el-icon class="el-icon--right"><component :is="'ele-ArrowDown'" /></el-icon></el-button>
-                <template #dropdown>
-                  <el-dropdown-menu>
-                    <el-dropdown-item v-if="auth(perms.softDelete)" @click.stop="onSoftDelete(row)" icon="ele-DeleteFilled">软删除</el-dropdown-item>
-                    <el-dropdown-item v-if="auth(perms.delete)" @click.stop="onDelete(row)" icon="ele-Delete">删除</el-dropdown-item>
-                  </el-dropdown-menu>
-                </template>            
-              </el-dropdown>
-              <span v-else style="margin-left:5px;height:inherit">
-                <el-button text type="warning" v-if="auth(perms.softDelete)" style="height:inherit" @click.stop="onDelete(row)" icon="ele-DeleteFilled">软删除</el-button>
-                <el-button text type="danger" v-if="auth(perms.delete)" style="height:inherit" @click.stop="onDelete(row)" icon="ele-Delete">删除</el-button>
-              </span>
-            </template>
-          </el-table-column>
+      <el-table v-loading="state.loading" :data="state.devProjectListData" row-key="id" ref="listTableRef"
+        @row-click="listTableToggleSelection" @selection-change="selsChange">
+
+        <el-table-column type="selection" width="50" />
+        <el-table-column prop="name" label="项目名称" show-overflow-tooltip width />
+        <el-table-column prop="code" label="项目编码" show-overflow-tooltip width />
+        <el-table-column prop="isEnable" label="是否启用" show-overflow-tooltip width />
+        <el-table-column prop="groupId_Text" label="使用模板组" show-overflow-tooltip width />
+        <el-table-column prop="remark" label="备注" show-overflow-tooltip width />
+        <el-table-column v-auths="[perms.update, perms.softDelete, perms.delete]" label="操作" :width="actionColWidth"
+          fixed="right">
+          <template #default="{ row }">
+            <el-button v-auth="perms.update" icon="ele-EditPen" text type="primary"
+              @click.stop="onEdit(row)">编辑</el-button>
+            <el-dropdown v-if="authAll([perms.delete, perms.softDelete])">
+              <el-button icon="el-icon--right" text type="warning">操作 <el-icon class="el-icon--right">
+                  <component :is="'ele-ArrowDown'" />
+                </el-icon></el-button>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item @click.stop="genCode(row)" icon="ele-Position">生成代码</el-dropdown-item>
+                  <el-dropdown-item v-if="auth(perms.softDelete)" @click.stop="onSoftDelete(row)"
+                    icon="ele-DeleteFilled">软删除</el-dropdown-item>
+                  <el-dropdown-item v-if="auth(perms.delete)" @click.stop="onDelete(row)"
+                    icon="ele-Delete">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+            <span v-else style="margin-left:5px;height:inherit">
+              <el-button text type="warning" v-if="auth(perms.softDelete)" style="height:inherit"
+                @click.stop="onDelete(row)" icon="ele-DeleteFilled">软删除</el-button>
+              <el-button text type="danger" v-if="auth(perms.delete)" style="height:inherit" @click.stop="onDelete(row)"
+                icon="ele-Delete">删除</el-button>
+            </span>
+          </template>
+        </el-table-column>
       </el-table>
 
       <div class="my-flex my-flex-end" style="margin-top: 20px">
-        <el-pagination
-          v-model:currentPage="state.pageInput.currentPage"
-          v-model:page-size="state.pageInput.pageSize"
-          :total="state.total"
-          :page-sizes="[10, 20, 50, 100]"
-          background
-          @size-change="onSizeChange"
-          @current-change="onCurrentChange"
-          layout="total, sizes, prev, pager, next, jumper"
-        />
+        <el-pagination v-model:currentPage="state.pageInput.currentPage" v-model:page-size="state.pageInput.pageSize"
+          :total="state.total" :page-sizes="[10, 20, 50, 100]" background @size-change="onSizeChange"
+          @current-change="onCurrentChange" layout="total, sizes, prev, pager, next, jumper" />
       </div>
     </el-card>
 
@@ -83,10 +90,11 @@
 
 <script lang="ts" setup name="dev/dev-project">
 import { ref, reactive, onMounted, getCurrentInstance, onBeforeMount, defineAsyncComponent, computed } from 'vue'
-import { PageInputDevProjectGetPageInput, DevProjectGetPageInput, DevProjectGetPageOutput, DevProjectGetOutput, DevProjectAddInput, DevProjectUpdateInput,
+import {
+  PageInputDevProjectGetPageInput, DevProjectGetPageInput, DevProjectGetPageOutput, DevProjectGetOutput, DevProjectAddInput, DevProjectUpdateInput,
   DevProjectGetListInput, DevProjectGetListOutput,
   DevGroupGetListOutput,
-  DevGroupGetOutput,                    
+  DevGroupGetOutput,
 } from '/@/api/dev/data-contracts'
 import { DevProjectApi } from '/@/api/dev/DevProject'
 import { DevGroupApi } from '/@/api/dev/DevGroup'
@@ -103,12 +111,12 @@ const listTableRef = ref()
 
 //权限配置
 const perms = {
-  add:'api:dev:dev-project:add',
-  update:'api:dev:dev-project:update',
-  delete:'api:dev:dev-project:delete',
-  batDelete:'api:dev:dev-project:batch-delete',
-  softDelete:'api:dev:dev-project:soft-delete',
-  batSoftDelete:'api:dev:dev-project:batch-soft-delete',
+  add: 'api:dev:dev-project:add',
+  update: 'api:dev:dev-project:update',
+  delete: 'api:dev:dev-project:delete',
+  batDelete: 'api:dev:dev-project:batch-delete',
+  softDelete: 'api:dev:dev-project:soft-delete',
+  batSoftDelete: 'api:dev:dev-project:batch-soft-delete',
 }
 
 const actionColWidth = authAll([perms.update, perms.softDelete]) || authAll([perms.update, perms.delete]) ? 140 : 75
@@ -184,7 +192,7 @@ const onDelete = (row: DevProjectGetOutput) => {
       await new DevProjectApi().delete({ id: row.id }, { loading: true, showSuccessMessage: true })
       onQuery()
     })
-    .catch(() => {})
+    .catch(() => { })
 }
 
 const onSizeChange = (val: number) => {
@@ -201,27 +209,27 @@ const selsChange = (vals: DevProjectGetPageOutput[]) => {
 }
 
 const onBatchDelete = async () => {
-  proxy.$modal?.confirmDelete(`确定要删除选择的${state.sels.length}条记录？`).then(async () =>{
-    const rst = await new DevProjectApi().batchDelete(state.sels?.map(item=>item.id) as number[], { loading: true, showSuccessMessage: true })
-    if(rst?.success){
+  proxy.$modal?.confirmDelete(`确定要删除选择的${state.sels.length}条记录？`).then(async () => {
+    const rst = await new DevProjectApi().batchDelete(state.sels?.map(item => item.id) as number[], { loading: true, showSuccessMessage: true })
+    if (rst?.success) {
       onQuery()
     }
   })
 }
 
 const onSoftDelete = async (row: DevProjectGetOutput) => {
-  proxy.$modal?.confirmDelete(`确定要移入回收站？`).then(async () =>{
+  proxy.$modal?.confirmDelete(`确定要移入回收站？`).then(async () => {
     const rst = await new DevProjectApi().softDelete({ id: row.id }, { loading: true, showSuccessMessage: true })
-    if(rst?.success){
+    if (rst?.success) {
       onQuery()
     }
   })
 }
 
 const onBatchSoftDelete = async () => {
-  proxy.$modal?.confirmDelete(`确定要将选择的${state.sels.length}条记录移入回收站？`).then(async () =>{
+  proxy.$modal?.confirmDelete(`确定要将选择的${state.sels.length}条记录移入回收站？`).then(async () => {
     const rst = await new DevProjectApi().batchSoftDelete(state.sels?.map(item => item.id) as number[], { loading: true, showSuccessMessage: true })
-    if(rst?.success){
+    if (rst?.success) {
       onQuery()
     }
   })
@@ -229,4 +237,17 @@ const onBatchSoftDelete = async () => {
 const listTableToggleSelection = async (row: any) => {
   listTableRef.value!.toggleRowSelection(row)
 }
+const genCode = async (row: DevProjectGetOutput) => {
+  await new DevProjectApi().generate({ id: row.id, groupId: row.groupId }, { loading: true, showSuccessMessage: true })
+}
+
+const batchGenCode = async () => {
+  if (state.sels.length == 0)
+    return proxy.$modal.msgWarning('请选择要生成的项目')
+
+  await new DevProjectApi().batchGenerate(state.sels.map(s => s.id) as number[], {
+    groupId: state.sels[0].groupId
+  }, { loading: true, showSuccessMessage: true })
+}
+
 </script>
